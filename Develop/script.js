64b11e8155e5e401f0a372ece40905efa189d6f9
ber <ber@berdeMacBook-Pro.local>
2022-12-20 20:19:29 -0800

    code update

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,24 @@
 // Assignment code here
+function generatePassword() {
+  var lowerCase = confirm("Do you like to include lowercase letter?");
+  var upperCase = confirm("Do you like to include uppercase letter?");
+  var number = confirm("Do you like to include number?");
+  var specialCharacter = confirm("Do you like to include sepcial characters?");
+  var codeLength  = prompt("How many digits do you want to include in you ")
 
+  if ((codeLength < 8 ) | (codeLength > 16) | (codeLength.typeof !== number)) {
+    confirm("Please enter again");
+  } else {
+    return codeLength;
+  }
+
+  if (lowerCase & upperCase & number & specialCharacter == false) {
+    txt = "You have to pick one!"; 
+  } else if ((lowerCase) && (upperCase = false) && (number = false) && (specialCharacter = false)) {
+    
+  return newPasscode;
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
